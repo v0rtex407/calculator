2db60774c03aa9410d1d44dd0ff30772b9627574
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-07-27 19:06:55 +0200

    Update service.js

--- service.js
+++ service.js
@@ -1,15 +1,13 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service.js').then(function (registration) {}).catch(function (error) {});
+}
 const cacheName = "v1"
 const cachedFiles = ["index.html", "calculator.css", "math.js", "favicon.ico"]
 self.addEventListener("install", (e) => {
-    console.log("Service Worker : Installed")
     e.waitUntil(
         caches.open(cacheName).then(cache => {
-            console.log("cached");
             cache.addAll(cachedFiles);
-        })
-    ).then(() => self.skipWaiting());
+        }).then(() => self.skipWaiting())
+    )
 })
-
-self.addEventListener("activate", () => {
-    console.log("Service Worker : Activated")
-})
+self.addEventListener("activate", () => {})
