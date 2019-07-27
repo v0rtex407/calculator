43b43e1b30de03913ad21e6794c789d66b345d83
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-07-27 18:26:09 +0200

    Update service.js

--- service.js
+++ service.js
@@ -1,12 +1,13 @@
 const cacheName = "v1"
-const cachedFiles = ["/index.html", "/calculator.css", "/math.js", "/favicon.ico"]
+const cachedFiles = ["index.html", "calculator.css", "math.js", "favicon.ico"]
 self.addEventListener("install", (e) => {
     console.log("Service Worker : Installed")
-    e.waitUntil(caches.open(cacheName).then(cache => {
+    caches.open(cacheName).then(cache => {
         console.log("cached");
-        cache.addAll(cachedFiles)
-    }).then(() => self.skipWaiting()))
-})
+        cache.addAll(cachedFiles);
+    })
+    ;
+});
 
 self.addEventListener("activate", (e) => {
     console.log("Service Worker : Activated")
