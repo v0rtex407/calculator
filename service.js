2047670408be2732a99b32966e37c714f52305be
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-07-27 18:14:12 +0200

    Add files via upload

---
+++ service.js
@@ -0,0 +1,12 @@
+const cacheName = "v1"
+const cachedFiles = ["index.html", "calculator.css", "math.js", "favicon.ico"]
+self.addEventListener("install", (e) => {
+    console.log("Service Worker : Installed")
+    e.waitUntil(caches.open(cacheName).then(cache => {
+        cache.addAll(cachedFiles)
+    }).then(() => self.skipWaiting()))
+})
+
+self.addEventListener("activate", (e) => {
+    console.log("Service Worker : Activated")
+})
