c5f098658369d1ef8bc59c920462036fc483641f
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-07-27 18:50:26 +0200

    Update service.js

--- service.js
+++ service.js
@@ -1,9 +1,16 @@
+if ('serviceWorker' in navigator) {
+        // Path che contiene il service worker
+        navigator.serviceWorker.register('service.js').then(function(registration) {
+          console.log('Service worker installato correttamente, ecco lo scope:', registration.scope);
+        }).catch(function(error) {
+          console.log('Installazione service worker fallita:', error);
+        });
 const cacheName = "v1"
 const cachedFiles = ["index.html", "calculator.css", "math.js", "favicon.ico"]
 self.addEventListener("install", (e) => {
     console.log("Service Worker : Installed")
     e.waitUntil(
-        cacheStorage.open(cacheName).then(cache => {
+        caches.open(cacheName).then(cache => {
             console.log("cached");
             cache.addAll(cachedFiles);
         })
